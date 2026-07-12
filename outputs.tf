37dd6a2e9847d8940e9324797bd55513f17b68c2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "dev_center_galleries" {
-  description = "All dev_center_gallery resources"
-  value       = azurerm_dev_center_gallery.dev_center_galleries
-}
 output "dev_center_galleries_dev_center_id" {
-  description = "List of dev_center_id values across all dev_center_galleries"
-  value       = [for k, v in azurerm_dev_center_gallery.dev_center_galleries : v.dev_center_id]
+  description = "Map of dev_center_id values across all dev_center_galleries, keyed the same as var.dev_center_galleries"
+  value       = { for k, v in azurerm_dev_center_gallery.dev_center_galleries : k => v.dev_center_id }
 }
 output "dev_center_galleries_name" {
-  description = "List of name values across all dev_center_galleries"
-  value       = [for k, v in azurerm_dev_center_gallery.dev_center_galleries : v.name]
+  description = "Map of name values across all dev_center_galleries, keyed the same as var.dev_center_galleries"
+  value       = { for k, v in azurerm_dev_center_gallery.dev_center_galleries : k => v.name }
 }
 output "dev_center_galleries_shared_gallery_id" {
-  description = "List of shared_gallery_id values across all dev_center_galleries"
-  value       = [for k, v in azurerm_dev_center_gallery.dev_center_galleries : v.shared_gallery_id]
+  description = "Map of shared_gallery_id values across all dev_center_galleries, keyed the same as var.dev_center_galleries"
+  value       = { for k, v in azurerm_dev_center_gallery.dev_center_galleries : k => v.shared_gallery_id }
 }
 
