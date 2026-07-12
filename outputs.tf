b3e9bc70cba3c1a6bbe9e4c93e91857856247bd8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "dev_center_galleries_id" {
+  description = "Map of id values across all dev_center_galleries, keyed the same as var.dev_center_galleries"
+  value       = { for k, v in azurerm_dev_center_gallery.dev_center_galleries : k => v.id }
+}
 output "dev_center_galleries_dev_center_id" {
   description = "Map of dev_center_id values across all dev_center_galleries, keyed the same as var.dev_center_galleries"
   value       = { for k, v in azurerm_dev_center_gallery.dev_center_galleries : k => v.dev_center_id }
